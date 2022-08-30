ac8621c8fe91f54ab6c9e2625cfe36e1fb7e5f9f
Uka0001 <ukraine0001@gmail.com>
2022-08-30 23:28:33 +0300

    initial commit

---
+++ src/main/java/com/uka0001/pethtml_parser/PetHtmlParserApplication.java
@@ -0,0 +1,13 @@
+package com.uka0001.pethtml_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHtmlParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetHtmlParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uka0001/pethtml_parser/PetHtmlParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uka0001.pethtml_parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHtmlParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
